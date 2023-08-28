47ac4ee968aca97ea8dacf8f250338831b4f0e4e
ppa-avensys <142867384+ppa-avensys@users.noreply.github.com>
2023-08-28 16:16:52 +0530

    comment

--- src/main/java/com/avensys/rts/constants/Constants.java
+++ src/main/java/com/avensys/rts/constants/Constants.java
@@ -2,6 +2,12 @@ package com.avensys.rts.constants;
 
 import org.springframework.stereotype.Component;
 
+/**
+ * 
+ * @author Pranay.Patadiya
+ * Constant file
+ */
+
 @Component
 public interface Constants {
 
